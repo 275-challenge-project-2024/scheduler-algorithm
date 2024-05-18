e64368435cae3ef84f9fe556cb2c7221ab9f18c2
subramanyajagadeesh <subramanya.grj@gmail.com>
2024-05-17 18:58:36 -0700

    Pushed more changes

--- Scheduler.cpp
+++ Scheduler.cpp
@@ -5,21 +5,22 @@
 #include <pthread.h>
 #include <tuple>
 
-#include "worker_heap.h"
-#include "task_heap.h"
+#include "../registry/worker_heap.h"
+#include "../registry/task_heap.h"
 #include "task_worker_mapping.h"
 
 #include <memory>
 #include <string>
 #include <grpcpp/grpcpp.h>
-#include "../worker-derian/task.grpc.pb.h"
+#include "task.grpc.pb.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
-using taskscheduler::Task;
 using taskscheduler::TaskStatus;
 using taskscheduler::TaskScheduler;
+using TASK_HEAP_H::task_heap_pop;
+using TASK_HEAP_H::task_heapify_down;
 
 #define verbose 1
 
@@ -33,12 +34,12 @@ public:
         : stub_(TaskScheduler::NewStub(channel)) {}
 
     Status SubmitTask(TaskElement task) {
-        TaskStatus status;
+        Status status;
         ClientContext context;
 
         Status grpcStatus = stub_->SubmitTask(&context, task, &status);
 
-        return status
+        return status;
     }
  
 private:
@@ -51,16 +52,16 @@ public:
     int worker_shm_id;
     int task_shm_id;
     int asgnd_tasks_shm_id;
-    HeapData* worker_heap;
+    WorkerHeapData* worker_heap;
     TaskHeapData* task_heap;
     TaskWorkerMappingList* asgnd_tasks_list;
    Scheduler() {
-        worker_shm_id = shmget(worker_heap_k, sizeof(HeapData), 0666);
+        worker_shm_id = shmget(worker_heap_k, sizeof(WorkerHeapData), 0666);
         if (worker_shm_id == -1) {
             perror("shmget for worker queue failed in Scheduler constructor");
             exit(1); 
         }
-        worker_heap = (HeapData*)shmat(worker_shm_id, NULL, 0);
+        worker_heap = (WorkerHeapData*)shmat(worker_shm_id, NULL, 0);
         if (worker_heap == (void*) -1) {
             perror("shmat for worker queue failed in Scheduler constructor");
             exit(1);
@@ -96,13 +97,13 @@ public:
     //     }
     // }
 
-    HeapElement getWorkerWithMaxCapacity() {
+    WorkerHeapElement getWorkerWithMaxCapacity() {
         pthread_mutex_lock(&this->worker_heap->mutex);
         if(worker_heap->size == 0) {
             pthread_mutex_unlock(&this->worker_heap->mutex);
-            return nullptr;
+            return NULL;
         }
-        HeapElement topWorker = worker_heap->data[0];
+        WorkerHeapElement topWorker = worker_heap->data[0];
         pthread_mutex_unlock(&this->worker_heap->mutex);
         return topWorker;
     }
@@ -111,20 +112,20 @@ public:
         pthread_mutex_lock(&this->task_heap->mutex);
         if(task_heap->size == 0) {
             pthread_mutex_unlock(&this->task_heap->mutex);
-            return nullptr;
+            return NULL;
         }
-        TaskElement topTask = heap_pop(this->task_heap);
-        heapify_down(this->task_heap, 0);
+        TaskElement topTask = task_heap_pop(this->task_heap);
+        task_heapify_down(this->task_heap, 0);
         pthread_mutex_unlock(&this->task_heap->mutex);
         return topTask;
     }
 
-    void pushAssignedTaskToWorker(TaskElement task, HeapElement worker){
+    void pushAssignedTaskToWorker(TaskElement task, WorkerHeapElement worker){
         TaskWorkerMapping taskMap;
-        taskMap.clientIp = task.clientIp;
+        taskMap.clientIp = &task.clientIp;
         taskMap.priority = task.priority;
-        taskMap.taskId = task.taskId;
-        taskMap.workerId = worker.workerId;
+        taskMap.taskId = &task.taskId;
+        taskMap.workerId = &worker.workerId;
         TaskSchedulerClient client(grpc::CreateChannel(worker.workerId, grpc::InsecureChannelCredentials()));
         Status response = client.SubmitTask(task);
         if (response.ok()){
@@ -148,7 +149,7 @@ int main(){
             std::this_thread::sleep_for(std::chrono::milliseconds(sleepTime));
             topTask = sch.getTaskWithMaxPriority();
         }
-        HeapElement worker = sch.getWorkerWithMaxCapacity();
+        WorkerHeapElement worker = sch.getWorkerWithMaxCapacity();
         while(worker == nullptr){
             std::this_thread::sleep_for(std::chrono::milliseconds(sleepTime));
             worker = sch.getWorkerWithMaxCapacity();
--- task_worker_mapping.h
+++ task_worker_mapping.h
@@ -19,15 +19,7 @@ struct TaskWorkerMapping {
 struct TaskWorkerMappingList {
     TaskWorkerMapping mappings[MAX_MAPPINGS];
     size_t size;
+    pthread_mutex_t mutex;
 };
-
-// Forward declaration of SharedMemory and SharedMemoryMapping structures
-struct SharedMemory;
-struct SharedMemoryMapping;
-
-// Function declarations for task-worker mapping
-TaskWorkerMappingList* initialize_task_worker_mapping(void* shm_addr);
-void add_task_worker_mapping(TaskWorkerMappingList* list, const TaskElement& task, const char* workerId);
-void update_task_status(SharedMemory* shared_memory, SharedMemoryMapping* shared_memory_mapping, const char* taskId, const char* new_status);
 
 #endif // TASK_WORKER_MAPPING_H
